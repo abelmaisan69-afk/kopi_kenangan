375bb0201a6d0e0936c3b411cf1d4b5764aaecb9
radhitsonabel-creator <radhitsonabel@gmail.com>
2026-04-02 11:57:58 +0700

    perubahana scrolling

--- js/script.js
+++ js/script.js
@@ -13,3 +13,12 @@ document.addEventListener('click', function(e){
         navbarNav.classList.remove('active')
     }
 });
+
+// smooth scroll untuk semua link yang href-nya #
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault(); // cegah reload
+        const target = document.querySelector(this.getAttribute('href'));
+        if(target) target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
