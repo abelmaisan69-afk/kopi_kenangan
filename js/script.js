0b5529cce6736c29a8325d8abbdf741cfd3162a8
radhitsonabel-creator <radhitsonabel@gmail.com>
2026-04-02 09:06:26 +0700

    update pertama

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// klik di luar bisa hilang
+document.addEventListener('click', function(e){
+    const hamburger = document.querySelector('#hamburger-menu');
+
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active')
+    }
+});
